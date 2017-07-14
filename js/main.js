7baaf90279da4d5b2d3115a2b9c9c57b259ace48
Gonzalo Rodriguez <g@stm.pm>
2017-07-14 14:57:06 -0500

    changes to back ground  and header layout

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+// Heading Transition
+	// (function() {
+	// 	var header = $('.header-caption');
+	// 	var range = 200;
+	// 	$(window).on('scroll', function () {1
+	// 		var scrollTop = $(this).scrollTop();
+	// 		var offset = header.offset().top;
+	// 		var height = header.outerHeight();
+	// 		offset = offset + height / 2;
+	// 		var calc = 1 - (scrollTop - offset + range) / range;
+	// 		header.css({opacity: calc});
+	// 		if (calc > '1') {
+	// 			header.css({opacity: 1});
+	// 		} else if (calc < '0') {
+	// 			header.css({opacity: 0});
+	// 		}
+	// 	});
+	// }());
