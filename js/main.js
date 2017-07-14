09a94ab81c2d254f14007f3b0eeadafd0cb10ccb
Gonzalo Rodriguez <g@stm.pm>
2017-07-14 17:24:23 -0500

    hamburger menu chages

--- js/main.js
+++ js/main.js
@@ -28,4 +28,59 @@ function closeNav() {
 	// }());
 
 
-  
+  function changeIcon(state) {
+  var barH = $("#hamburger-icon .top").height();
+  var barM = parseInt($("#hamburger-icon .top").css('marginBottom'));
+  midY = barM + barH;
+
+  if (state === "ham") {
+    $("#hamburger-icon .top").stop().transition({
+      rotate: 0
+    }, function() {
+      $("#hamburger-icon .top").stop().transition({
+        y: 0
+      });
+    });
+    $("#hamburger-icon .bottom").stop().transition({
+      rotate: 360
+    }, function() {
+      $("#hamburger-icon .bottom").stop().transition({
+        y: 0
+      });
+      $("#hamburger-icon .middle").css({
+        opacity: 1.0
+      });
+    });
+  } else if (state === "noham") {
+    $("#hamburger-icon .middle").css({
+      rotate: 360
+    });
+    $("#hamburger-icon .top").stop().transition({
+      y: midY
+    }, function() {
+      $("#hamburger-icon .middle").css({
+        opacity: 0
+      });
+      $("#hamburger-icon .top").stop().transition({
+        rotate: 45
+      });
+    });
+    $("#hamburger-icon .bottom").stop().transition({
+      y: -midY
+    }, function() {
+      $("#hamburger-icon .bottom").stop().transition({
+        rotate: 315
+      });
+    });
+  }
+}
+
+$(document).on('click', '#hamburger-icon', function() {
+  if ($(this).hasClass("noham")) {
+    $(this).removeClass().addClass("ham");
+    changeIcon("ham");
+  } else {
+    $(this).removeClass().addClass("noham");
+    changeIcon("noham");
+  }
+});
